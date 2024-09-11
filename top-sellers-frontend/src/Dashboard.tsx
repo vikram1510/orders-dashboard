e8b5e474b57b6a99c9674bd5ef2803c368c7fbaf
Vikram Bageja <vikram.bageja@gmail.com>
2024-09-11 16:49:46 +0100

    Clean up

--- top-sellers-frontend/src/Dashboard.tsx
+++ top-sellers-frontend/src/Dashboard.tsx
@@ -3,24 +3,6 @@ import { LoadingIcon } from "./Icons"
 import { toDollars } from "./utils/toDollars"
 import { useDashboard } from "./hooks/useDashboard"
 
-// Overview:
-// You are provided with an incomplete <Dashboard /> component.
-// Demo video - You can view how the completed functionality should look at: https://drive.google.com/file/d/1lH8ojlvb62I_9z3jGxhNEY_-8S4trNo7/view?usp=sharing
-// This demo video uses the same dataset, so your total and ranking calculations should match it
-// You are not allowed to add any additional HTML elements.
-// You are not allowed to use refs.
-
-// Requirements:
-// Once the <Dashboard /> component is mounted, load the order data using the getOrders function imported above
-// Once all the data is successfully loaded, hide the loading icon
-// Calculate and display the total revenue
-// Display a ranking showing the sellers ordered by their total revenue using the <SellerRanking /> component.
-// The seller with the highest revenue should be shown at the top with position 1.
-// All the revenue values should only consider Confirmed orders. Canceled orders should be ignored.
-// All dollar amounts should be displayed to 2 decimal places
-// The getOrders function times out frequently. Display any errors returned while loading the data in the provided div.
-// The retry button should clear the error and reattempt the request
-
 type SellerRankingProps = {
   position: number
   sellerName: string
@@ -51,7 +33,6 @@ const Dashboard = () => {
       </header>
       <main>
         {loading && <LoadingIcon />}
-        {/* Place any data fetching errors inside this div, only render the div if there are errors */}
 
         {error && (
           <div data-testid="error-container" className={styles.errorContainer}>
